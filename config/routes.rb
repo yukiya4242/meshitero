de6585ffddc41f512fe18187f9727df2a588ba34
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-29 12:58:07 +0000

    until_edit_post_comments_controller_create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  resources :post_images, only:[:index, :show, :new, :create, :destroy]
+  resources :post_images, only:[:index, :show, :new, :create, :destroy] do
+    resources :post_comments, only:[:create]
+  end
   resources :users,       only:[:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
