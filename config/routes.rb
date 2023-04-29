30613fa562260ee5a55655f2ff2bbfab96d4776f
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-04-29 12:35:15 +0000

    none

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
+
   devise_for :users
 
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :post_images, only:[:index, :show, :new, :create, :destroy]
+  resources :users,       only:[:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
